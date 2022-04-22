27182b6b0f725a44669df6d62320e7440f7a6c95
PJColombo <paulo.colombo@pm.me>
2022-04-22 20:54:35 +0200

    feat: add title

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,15 +1,20 @@
-import { GU } from "@1hive/1hive-ui";
+import { GU, textStyle } from "@1hive/1hive-ui";
 import styled from "styled-components";
 import { AccountModule } from "./AccountModule";
 
 export const TopBar = () => (
   <Container>
+    <Title>Superfluid Vortex</Title>
     <AccountModule compact={false} />
   </Container>
 );
 
 const Container = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   padding: ${2 * GU}px;
+`;
+
+const Title = styled.div`
+  ${textStyle("title2")};
 `;
